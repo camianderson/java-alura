0dc75aaa0eef7158b49bf2c99ff890de7beb9925
Cami Anderson <camidantas@icloud.com>
2023-03-28 16:38:42 -0400

    Add class to generate sticker

---
+++ src/StickerGenerator.java
@@ -0,0 +1,16 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class StickerGenerator {
+    
+    public void generate() throws Exception {
+
+        //read the image
+        BufferedImage originalImg = ImageIO.read(new File("img/movie.jpeg"));
+
+
+    }
+
+}
