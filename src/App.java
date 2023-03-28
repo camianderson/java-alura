5562bb0de0de80cef353d00f723c9faf2e4b30dd
Cami Anderson <camidantas@icloud.com>
2023-03-28 10:51:37 -0400

    Add basic structure

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -15,6 +17,10 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
+
+        // select only the data we want (title, poster, rating)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
     
     }
 }
