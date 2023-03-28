f3de43f710c7cdf72007c558216fed04dd789dfb
Cami Anderson <camidantas@icloud.com>
2023-03-28 16:41:50 -0400

    Add method to create the new image

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -10,7 +10,11 @@ public class StickerGenerator {
         //read the image
         BufferedImage originalImg = ImageIO.read(new File("img/movie.jpeg"));
 
-
+        //create the new image in the memory with transparency and new size
+        int width = originalImg.getWidth();
+        int height = originalImg.getHeight();
+        int newHeight = height + 200;
+        BufferedImage newImg = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
     }
 
 }
