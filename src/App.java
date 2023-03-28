4619edb393862cd0e686e53b347d8cae398cb30b
Cami Anderson <camidantas@icloud.com>
2023-03-28 10:48:50 -0400

    Add GET call to Movies API

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
-tpublic class App {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
     public static void main(String[] args) throws Exception {
 
+        // call the API to get the data (response body) and get top 250 movies
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
     
     }
 }
