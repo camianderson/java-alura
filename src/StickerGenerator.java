df55874bbab4c95554c3d0d2cfc269506585fd6b
Cami Anderson <camidantas@icloud.com>
2023-03-29 09:45:42 -0400

    Remove testing code and make it dynamic to use API data

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 import java.awt.Color;
 import java.awt.Font;
 
@@ -8,10 +9,10 @@ import javax.imageio.ImageIO;
 
 public class StickerGenerator {
     
-    public void generate() throws Exception {
+    public void generate(InputStream inputStream, String nameFile) throws Exception {
 
         //read the image
-        BufferedImage originalImg = ImageIO.read(new File("img/movie.jpeg"));
+        BufferedImage originalImg = ImageIO.read(inputStream);
 
         //create the new image in the memory with transparency and new size
         int width = originalImg.getWidth();
@@ -32,11 +33,7 @@ public class StickerGenerator {
         graphics.drawString("AWESOME", 180, newHeight-25);
 
         //write the new image in a file
-        ImageIO.write(newImg, "png", new File("exit/sticker.png"));
+        ImageIO.write(newImg, "png", new File(nameFile));
     }
 
-    public static void main(String[] args) throws Exception {
-        var creator = new StickerGenerator();
-        creator.generate();
-    }
 }
