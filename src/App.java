0a81a796579351e03f7b02ded09f90a0dfaa74b2
Cami Anderson <camidantas@icloud.com>
2023-03-30 11:03:53 -0400

    Add ClientHttp and refactor App.java to call it

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -12,24 +7,23 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // call the API to get the data (response body) and get top movies
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI address = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        
+       var http = new ClientHttp();
+       String json = http.dataSearch(url);
 
         // instatiate the StickerGenerator to create the stickers
         var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body);
-        for (Map<String,String> movie: movieList) {
-            InputStream inputStream = new URL(movie.get("image")).openStream();
-            String stickerTitle = movie.get("title") + ".png";
+        List<Map<String, String>> contentList = parser.parse(json);
+        for (Map<String,String> content: contentList) {
+            InputStream inputStream = new URL(content.get("image")).openStream();
+            String stickerTitle = content.get("title") + ".png";
 
             var generator = new StickerGenerator();
             generator.generate(inputStream, stickerTitle);
 
-            System.out.println("\u001b[1mTitle:\u001b[m " + movie.get("title"));
+            System.out.println("\u001b[1mTitle:\u001b[m " + content.get("title"));
             System.out.println();
 
         }
---
+++ src/ClientHttp.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ClientHttp {
+
+    public String dataSearch (String url){
+
+        try {
+
+            URI address = URI.create(url);
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(address).GET().build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            String body = response.body();
+            return body;
+
+        } catch (IOException | InterruptedException ex){
+            throw new RuntimeException(ex);
+        }
+
+    }
+}
