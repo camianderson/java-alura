efecbbb8bf02d682ea68b921ac552970dd418d9b
Cami Anderson <camidantas@icloud.com>
2023-03-30 08:20:14 -0400

    Remove unused code

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // call the API to get the data (response body) and get top 250 movies
+        // call the API to get the data (response body) and get top movies
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI address = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -19,7 +19,7 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        // select only the data we want (title, poster, rating)
+        // instatiate the StickerGenerator to create the stickers
         var parser = new JsonParser();
         List<Map<String, String>> movieList = parser.parse(body);
         for (Map<String,String> movie: movieList) {
@@ -29,17 +29,7 @@ public class App {
             var generator = new StickerGenerator();
             generator.generate(inputStream, stickerTitle);
 
-            // System.out.println("\u001b[1m\u001b[40mRank: " + movie.get("rank") + " \u001b[m");
             System.out.println("\u001b[1mTitle:\u001b[m " + movie.get("title"));
-            // System.out.println("\u001b[1mPoster URL:\u001b[m " + movie.get("image"));
-            // System.out.println("\u001b[1mRating:\u001b[m " + movie.get("imDbRating"));
-            // double rating = Double.parseDouble(movie.get("imDbRating"));
-            // int starNumber = (int) rating;
-            // for(int n = 1; n <= starNumber; n++){
-            //     System.out.print("🌟");
-            // }
-
-            // System.out.println("\n");
             System.out.println();
 
         }
