94a28e70cc37dbd67b0a9465910999fd6a0c220e
Cami Anderson <camidantas@icloud.com>
2023-03-30 11:25:25 -0400

    Add NasaContentExtractor

---
+++ src/NasaContentExtractor.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class NasaContentExtractor {
+
+    public List<Content> extractContent(String json) {
+
+        //extract the data I need
+        var parser = new JsonParser();
+        List<Map<String, String>> attributesList = parser.parse(json);
+
+        List<Content> allContent = new ArrayList<>();
+
+        //populate the content list
+        for (Map<String, String> attributes : attributesList) {
+            String title = attributes.get("title");
+            String urlImage = attributes.get("url");
+
+            var content = new Content(title, urlImage);
+
+            allContent.add(content);
+        }
+
+        return allContent;
+
+    }
+
+}
